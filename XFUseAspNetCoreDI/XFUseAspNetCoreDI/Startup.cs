8bd8874199cc6f515ea5a7ea6f68592684c2aea7
Shuhey <shuhey.dev@gmail.com>
2020-06-10 01:19:01 +0900

    appsettings.jsonからの値の取得の確認.

--- XFUseAspNetCoreDI/XFUseAspNetCoreDI/Startup.cs
+++ XFUseAspNetCoreDI/XFUseAspNetCoreDI/Startup.cs
@@ -1,17 +1,13 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
-using System.Net.Http;
 using System.Reflection;
-using System.Text;
 using Xamarin.Essentials;
-using Xamarin.Forms;
 using XFUseAspNetCoreDI.Services;
 using XFUseAspNetCoreDI.ViewModels;
 using XFUseAspNetCoreDI.Views;
+using Microsoft.Extensions.Configuration.EnvironmentVariables;
 
 namespace XFUseAspNetCoreDI
 {
@@ -25,8 +21,8 @@ namespace XFUseAspNetCoreDI
         /// <param name="nativeConfigureServices">ネイティブ側のConfigureServiceメソッドが渡される</param>
         public static App Init(Action<HostBuilderContext, IServiceCollection> nativeConfigureServices)
         {
-            var a = Assembly.GetExecutingAssembly();
-            using var stream = a.GetManifestResourceStream("XFUseAspNetCoreDI.appsettings.json");
+            var assm = Assembly.GetExecutingAssembly();
+            using var stream = assm.GetManifestResourceStream("XFUseAspNetCoreDI.appsettings.json");
 
             var host = new HostBuilder()
                 .ConfigureHostConfiguration(c =>
@@ -61,16 +57,26 @@ namespace XFUseAspNetCoreDI
             if (ctx.HostingEnvironment.IsDevelopment())
             {
                 services.AddSingleton<IDataService, MockDataService>();
+
+                //appsettings.json内の値を取得する
+                //1.GetValueメソッドを使う
+                var val1 = ctx.Configuration.GetValue<string>("Hello");// World
+                //2.Configurationからインデクサで取得
+                var val2 = ctx.Configuration["Hello"];// World
+                //3.2階層目の値を取得する
+                var val3 = ctx.Configuration["Xamarin:Forms"];//4.6
             }
             else
             {
                 services.AddSingleton<IDataService, DataService>();
             }
 
-            services.AddHttpClient("covid19_japan", c => {
+            services.AddHttpClient("covid19_japan", c =>
+            {
                 c.BaseAddress = new Uri("https://covid19-japan-web-api.now.sh/api//v1/");
             });
             //services.AddSingleton<HttpClient>();
+
 
             services.AddTransient<MainPageViewModel>();
             services.AddTransient<MainPage>();
